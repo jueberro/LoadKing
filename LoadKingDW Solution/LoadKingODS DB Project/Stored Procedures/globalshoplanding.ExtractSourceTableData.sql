3e686584c3b99d1b18b97adce7e9038e57eb305b
Bob Rubocki <brubocki@pragmaticworks.com>
2020-03-13 19:13:36 -0400

    Adding parameter to ExtractSourceTableData procedure and corresponding change in SSIS package that calls it.

--- LoadKingDW Solution/LoadKingODS DB Project/Stored Procedures/globalshoplanding.ExtractSourceTableData.sql
+++ LoadKingDW Solution/LoadKingODS DB Project/Stored Procedures/globalshoplanding.ExtractSourceTableData.sql
@@ -1,13 +1,38 @@
 ﻿CREATE PROCEDURE [globalshoplanding].[ExtractSourceTableData]
-			  @SourceTableName	NVARCHAR(100)
+			  @LoadLogKey		INT
+			, @SourceTableName	NVARCHAR(100)
 			, @TargetTableName	NVARCHAR(100)
 			, @ExtracLowDate	DATETIME2(7)  
 AS
 
 BEGIN
 
+	DECLARE @SourceRecordCount INT = 0
+
+	/*
+		This procedure was added as a stub/placeholder.  A few notes:
+		* This procedure will extract data from one Global Shop table 
+		   and insert it into a corresponding ODS table.
+		* The SELECT will include the LoadLogKey value (i.e. Batch ID), which will be 
+		   inserted into the ODS table.
+		* The SELECT will also inlcude a WHERE clause to only pull records where
+		   a last updated date (or similar) is greater/later than @ExtractLowDate
+		* Date format needs to be determined
+		* Paramaters and data types of parameters will likely change.
+		* It will return a 
+	*/
+
+--	INSERT INTO <TargetTableName> 
 	SELECT 1
 --	FROM	@SourceTableName
 --	WHERE	DATE_LAST_CHANGED >= @ExtractLowDate
+	
+
+
+	--Count records from INSERT/SELECT
+	SELECT @SourceRecordCount = @@ROWCOUNT
+
+	--Return one row, one field record set with SourceRecordCount
+	SELECT SourceRecordCount = @SourceRecordCount
 
 END
