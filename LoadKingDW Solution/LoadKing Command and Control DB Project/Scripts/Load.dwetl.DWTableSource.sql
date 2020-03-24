5c0437657984728b501ed7ce35f02a5e981dd269
Bob Rubocki <brubocki@pragmaticworks.com>
2020-03-24 14:57:32 -0400

    Adding table to manage DW table load configurations

---
+++ LoadKingDW Solution/LoadKing Command and Control DB Project/Scripts/Load.dwetl.DWTableSource.sql
@@ -0,0 +1,12 @@
+﻿
+IF NOT EXISTS(SELECT 1 FROM dwetl.DWTableSource 
+			WHERE	DWTableName			= 'dw.DimCustomer'
+			 AND	SourceSystemName	= 'GlobalShop'
+			 AND	SourceDataSetName	= 'Customer'
+			)
+
+BEGIN
+	INSERT INTO dwetl.DWTableSource (DWTableName, DWTableType, SourceSystemName, SourceDataSetName, SSISPackageName, StoredProcedureName)
+	VALUES ('dw.DimCustomer', 'Dimension', 'Global Shop', 'Customer', 'DWLoadDimCustomer.dtsx', 'dw.LoadDimCustomer')
+
+END
---
+++ LoadKingDW Solution/LoadKing Command and Control DB Project/Tables/dwetl.DWTableSource.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dwetl].[DWTableSource]
+(
+	[DWTableName]				NVARCHAR(100)		NOT NULL,
+	[DWTableType]				NVARCHAR(100)		NOT NULL,
+	[SourceSystemName]			NVARCHAR(100)		NOT NULL,
+	[SourceDataSetName]			NVARCHAR(100)		NOT NULL,
+	[SourceSQLString]			NVARCHAR(4000)			NULL,
+	[SSISPackageName]			NVARCHAR(100)			NULL,
+	[StoredProcedureName]		NVARCHAR(100)			NULL,
+	[LoadDWTableFlag]			BIT						NULL, -- A switch to enable/diable a table load
+
+	/*Temporal Table Requirements*/
+	[SysStartTime]	DATETIME2 GENERATED ALWAYS AS ROW START HIDDEN NOT NULL , 
+	[SysEndTime]	DATETIME2 GENERATED ALWAYS AS ROW END	HIDDEN NOT NULL , 
+	PERIOD FOR SYSTEM_TIME (SysStartTime, SysEndTime)
+)
+WITH	(
+			SYSTEM_VERSIONING = ON (HISTORY_TABLE = dwetl.DWTableSource_history) --New or existing table
+		)
+
+
+GO
+
+ALTER TABLE dwetl.DWTableSource
+ADD CONSTRAINT [PK_dwetl_DWTableSource] PRIMARY KEY ([DWTableName], [SourceSystemName], [SourceDataSetName])
+
+GO
+
+ALTER TABLE dwetl.DWTableSource
+ADD CONSTRAINT [DF_dwetl_DWTableSource_LoadDWTableFlag] DEFAULT (0) FOR LoadDWTableFlag
