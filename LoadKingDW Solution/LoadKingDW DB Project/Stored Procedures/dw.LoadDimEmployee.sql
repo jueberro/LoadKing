e521fbde629b868a57aad132db81153be83b7e17
Bob Rubocki <brubocki@pragmaticworks.com>
2020-04-02 10:15:40 -0400

    Beginning modifications to dw.LoadDimEmployee (NOT DONE)

--- LoadKingDW Solution/LoadKingDW DB Project/Stored Procedures/dw.LoadDimEmployee.sql
+++ LoadKingDW Solution/LoadKingDW DB Project/Stored Procedures/dw.LoadDimEmployee.sql
@@ -1,161 +1,69 @@
-﻿CREATE PROCEDURE [dw].[sp_LoadDimEmployee] AS
+﻿CREATE PROCEDURE [dw].[sp_LoadDimEmployee] @LoadLogKey INT  AS
 
 BEGIN
 
 	DECLARE @CurrentTimestamp DATETIME2(7)
 	SELECT @CurrentTimestamp = GETUTCDATE()
 
-	MERGE INTO dbo.Dim_Employee AS TRG
-	USING dwstage.V_EMPLOYEE_MSTR AS SRC
-	 ON	TRG.EMPLOYEE = SRC.EMPLOYEE 
-	  AND TRG.IsEffective = 'y'
+	BEGIN TRY DROP TABLE #DimEmployee_work END TRY BEGIN CATCH END CATCH
 
-	WHEN MATCHED
-	   AND (SRC.[RECORD_TYPE]		   <> TRG.[RECORD_TYPE]		  
-		  OR SRC.[NAME]			   <> TRG.[NAME]			  
-		  OR SRC.[ADDRESS]			   <> TRG.[ADDRESS]			  
-		  OR SRC.[CITY]			   <> TRG.[CITY]			  
-		  OR SRC.[STATE]			   <> TRG.[STATE]			  
-		  OR SRC.[ZIP_CODE]		   <> TRG.[ZIP_CODE]		  
-		  OR SRC.[PHONE]			   <> TRG.[PHONE]			  
-		  OR SRC.[SOCIAL_SECURITY_NO] <> TRG.[SOCIAL_SECURITY_NO]
-		  OR SRC.[SEX]				   <> TRG.[SEX]				  
-		  OR SRC.[BIRTHDATE]		   <> TRG.[BIRTHDATE]		  
-		  OR SRC.[DATE_HIRE]		   <> TRG.[DATE_HIRE]		  
-		  OR SRC.[DATE_TERMINATION]   <> TRG.[DATE_TERMINATION]  
-		  OR SRC.[DATE_RAISE]		   <> TRG.[DATE_RAISE]		  
-		  OR SRC.[PR_CARRY_OVER_YMD]  <> TRG.[PR_CARRY_OVER_YMD] 
-		  OR SRC.[ELIG_HOL_DATE]	   <> TRG.[ELIG_HOL_DATE]	  
-		  OR SRC.[ELIG_DT_401K]	   <> TRG.[ELIG_DT_401K]	  
-		  OR SRC.[COMMENTS_1]		   <> TRG.[COMMENTS_1]		  
-		  OR SRC.[COMMENTS_2]		   <> TRG.[COMMENTS_2]		  
-		  OR SRC.[COMMENTS_3]		   <> TRG.[COMMENTS_3]		  
-		  OR SRC.[RATE_PREVIOUS]	   <> TRG.[RATE_PREVIOUS]	  
-		  OR SRC.[DEPT_EMPLOYEE]	   <> TRG.[DEPT_EMPLOYEE]	  
-		  OR SRC.[WORKMAN_COMP]	   <> TRG.[WORKMAN_COMP]	  
-		  OR SRC.[PAY_TYPE]		   <> TRG.[PAY_TYPE]		  
-		  OR SRC.[FREQUENCY]		   <> TRG.[FREQUENCY]		  
-		  OR SRC.[RATE]			   <> TRG.[RATE]			  
-		  OR SRC.[SHIFT]			   <> TRG.[SHIFT]			  
-		  OR SRC.[DIFFERENTIAL]	   <> TRG.[DIFFERENTIAL]	  
-		  OR SRC.[MARITAL_STATUS]	   <> TRG.[MARITAL_STATUS]	  
-		  OR SRC.[STATE_CODE]		   <> TRG.[STATE_CODE]		  
-		  OR SRC.[SUI]				   <> TRG.[SUI]				  
-		  OR SRC.[EMAIL_ADDR]		   <> TRG.[EMAIL_ADDR]		  
-		  OR SRC.[ALPHA_SORT]		   <> TRG.[ALPHA_SORT]		  
-		  OR SRC.[VACATION_LEFT]	   <> TRG.[VACATION_LEFT]	 
-		  OR SRC.[SICK_LEFT]		   <> TRG.[SICK_LEFT]) 
+	--CREATE TEMP table
+	CREATE TABLE #DimEmployee_work (
+		[EmployeeID]				NCHAR (5)      NOT NULL,
+		[EmployeeRecordType]		NCHAR (1)      NULL,
+		[EmployeeName]				NVARCHAR (100) NULL,
+		[EmployeeAddress]			NVARCHAR (100) NULL,
+		[EmployeeCity]				NVARCHAR (50)  NULL,
+		[EmployeeState]				NCHAR (2)      NULL,
+		[EmployeePostalCode]		NCHAR (9)      NULL,
+		[EmployeeGender]			NCHAR (1)      NULL,
+		[EmployeeHireDate]			DATE			NULL,
+		[EmployeeTerminationDate]	DATE          NULL,
+		[EmployeeDepartment]		NCHAR (4)      NULL,
+		[EmployeeIsSalesperson]		BIT   null,
 
-		THEN --UPDATE/EXPIRE current version of records for which a new version will be INSERTED later
-			 UPDATE SET TRG.IsEffective = 'n', TRG.End_date =  @CurrentTimestamp        
+		/*Hashes used for identifying changes, not required for reporting*/
+		[Type1RecordHash]			VARCHAR(66)				NULL,	--66 allows for "0x" + 64 characater hash
+		[Type2RecordHash]			VARCHAR(66)				NULL,	--66 allows for "0x" + 64 characater hash
+
+		/*DW Metadata fields, not required for reporting*/
+		[SourceSystemName]			NVARCHAR(100)		NOT NULL,
+		[DWEffectiveStartDate]		DATETIME2(7)		NOT NULL,
+		[DWEffectiveEndDate]		DATETIME2(7)		NOT NULL,
+		[DWIsCurrent]				BIT					NOT NULL,
+
+		/*ETL Metadata fields, not required for reporting (DWEffectiveStartDate may not neccessarily be the same as RecordCreateDate, for example */
+		[LoadLogKey]					INT
+	)
 
 
-		WHEN NOT MATCHED --Insert *N E W* employee records
-		THEN 
-		INSERT 
-			(		
-			  [EMPLOYEE]
-			  ,[RECORD_TYPE]
-			  ,[NAME]
-			  ,[ADDRESS]
-			  ,[CITY]
-			  ,[STATE]
-			  ,[ZIP_CODE]
-			  ,[PHONE]
-			  ,[SOCIAL_SECURITY_NO]
-			  ,[SEX]
-			  ,[BIRTHDATE]
-			  ,[DATE_HIRE]
-			  ,[DATE_TERMINATION]
-			  ,[DATE_RAISE]
-			  ,[PR_CARRY_OVER_YMD]
-			  ,[ELIG_HOL_DATE]
-			  ,[ELIG_DT_401K]
-			  ,[COMMENTS_1]
-			  ,[COMMENTS_2]
-			  ,[COMMENTS_3]
-			  ,[RATE_PREVIOUS]
-			  ,[DEPT_EMPLOYEE]
-			  ,[WORKMAN_COMP]
-			  ,[PAY_TYPE]
-			  ,[FREQUENCY]
-			  ,[RATE]
-			  ,[SHIFT]
-			  ,[DIFFERENTIAL]
-			  ,[MARITAL_STATUS]
-			  ,[STATE_CODE]
-			  ,[SUI]
-			  ,[EMAIL_ADDR]
-			  ,[ALPHA_SORT]
-			  ,[VACATION_LEFT]
-			  ,[SICK_LEFT]
-			  ,[ETL_Batch]
-			  ,[ETL_Completed]
-			  ,[Effective_Date]
-			  ,[End_Date]
-			  ,IsEffective)
 
-			  VALUES(
-			  	SRC.[EMPLOYEE]
-			  ,SRC.[RECORD_TYPE]
-			  ,SRC.[NAME]
-			  ,SRC.[ADDRESS]
-			  ,SRC.[CITY]
-			  ,SRC.[STATE]
-			  ,SRC.[ZIP_CODE]
-			  ,SRC.[PHONE]
-			  ,SRC.[SOCIAL_SECURITY_NO]
-			  ,SRC.[SEX]
-			  ,SRC.[BIRTHDATE]
-			  ,SRC.[DATE_HIRE]
-			  ,SRC.[DATE_TERMINATION]
-			  ,SRC.[DATE_RAISE]
-			  ,SRC.[PR_CARRY_OVER_YMD]
-			  ,SRC.[ELIG_HOL_DATE]
-			  ,SRC.[ELIG_DT_401K]
-			  ,SRC.[COMMENTS_1]
-			  ,SRC.[COMMENTS_2]
-			  ,SRC.[COMMENTS_3]
-			  ,SRC.[RATE_PREVIOUS]
-			  ,SRC.[DEPT_EMPLOYEE]
-			  ,SRC.[WORKMAN_COMP]
-			  ,SRC.[PAY_TYPE]
-			  ,SRC.[FREQUENCY]
-			  ,SRC.[RATE]
-			  ,SRC.[SHIFT]
-			  ,SRC.[DIFFERENTIAL]
-			  ,SRC.[MARITAL_STATUS]
-			  ,SRC.[STATE_CODE]
-			  ,SRC.[SUI]
-			  ,SRC.[EMAIL_ADDR]
-			  ,SRC.[ALPHA_SORT]
-			  ,SRC.[VACATION_LEFT]
-			  ,SRC.[SICK_LEFT]
-			  ,SRC.[ETL_Batch]
-			  ,SRC.[ETL_Completed]
-			  , @CurrentTimestamp
-			  ,convert(datetime,'2100-01-01',120)
-			  , 'y'
-			  );
+	INSERT INTO #DimEmployee
+	SELECT 		
+		, [EmployeeID]				= CAST(EMPLOYEE AS NCHAR(5)
+		, [EmployeeRecordType]		= CAST(RECORD_TYPE AS NCHAR(1)
+		, [EmployeeName]			= CAST([NAME] AS NVARCHAR(100)
+		, [EmployeeAddress]			= 
+		, [EmployeeCity]				
+		, [EmployeeState]				
+		, [EmployeePostalCode]		
+		, [EmployeeGender]			
+		, [EmployeeHireDate]			
+		, [EmployeeTerminationDate]	
+		, [EmployeeDepartment]		
+		, [EmployeeIsSalesperson]		
+		
+		, [Type1RecordHash]			
+		, [Type2RecordHash]			
+		
+		, [SourceSystemName]			
+		, [DWEffectiveStartDate]		
+		, [DWEffectiveEndDate]		
+		, [DWIsCurrent]				
+		
+		, /*ETL Metadata fields, not rrtDate may not neccessarily be the same as RecordCreateDate, for example */
+		 [LoadLogKey]				
+	FROM  dwstage.EMPLOYEE_MSTR
 
 
 END
-
-
-
-
-
-/*
-
- - INSERT NEW Items
- - UPDATE existing dim records IF SRC member exists and one or more fields have changed (EffectiveEndDate, IsCurrent = N)
- - INSERT new version of matched record with new field values
-
-
-
-
-
-
-*/
-GO
-
