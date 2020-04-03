6ad2dea93e2f85d447799a2ee82edef92cb4cbb1
Bob Rubocki <brubocki@pragmaticworks.com>
2020-04-03 10:09:07 -0400

    Adding entries to dwetl.DWTableSource script

--- LoadKingDW Solution/LoadKing Command and Control DB Project/Scripts/Load.dwetl.DWTableSource.sql
+++ LoadKingDW Solution/LoadKing Command and Control DB Project/Scripts/Load.dwetl.DWTableSource.sql
@@ -1,12 +1,23 @@
-﻿
+﻿IF NOT EXISTS(SELECT 1 FROM dwetl.DWTableSource 
+			WHERE	DWTableName			= 'dw.DimEmployee'
+			 AND	SourceSystemName	= 'Global Shop'
+			 AND	SourceDataSetName	= 'Employee'
+			)
+
+BEGIN
+	INSERT INTO dwetl.DWTableSource (DWTableName, DWTableType, SourceSystemName, SourceDataSetName, StoredProcedureName)
+	VALUES ('dw.DimEmployee', 'Dimension', 'Global Shop', 'Employee', 'dw.LoadDimEmployee')
+
+END
+
 IF NOT EXISTS(SELECT 1 FROM dwetl.DWTableSource 
 			WHERE	DWTableName			= 'dw.DimCustomer'
-			 AND	SourceSystemName	= 'GlobalShop'
+			 AND	SourceSystemName	= 'Global Shop'
 			 AND	SourceDataSetName	= 'Customer'
 			)
 
 BEGIN
-	INSERT INTO dwetl.DWTableSource (DWTableName, DWTableType, SourceSystemName, SourceDataSetName, SSISPackageName, StoredProcedureName)
-	VALUES ('dw.DimCustomer', 'Dimension', 'Global Shop', 'Customer', 'DWLoadDimCustomer.dtsx', 'dw.LoadDimCustomer')
+	INSERT INTO dwetl.DWTableSource (DWTableName, DWTableType, SourceSystemName, SourceDataSetName, StoredProcedureName)
+	VALUES ('dw.DimCustomer', 'Dimension', 'Global Shop', 'Customer', 'dw.LoadDimCustomer')
 
 END
