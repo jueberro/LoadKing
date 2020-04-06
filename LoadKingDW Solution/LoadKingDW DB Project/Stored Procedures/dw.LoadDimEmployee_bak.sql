a58a95c17024e82986818b7c98f9eb8a82739b62
Joe Ueberroth <jueberroth@pragmaticworks.com>
2020-04-06 08:56:11 -0400

    Modified LoadDimEmployee Proc to use View

--- LoadKingDW Solution/LoadKingDW DB Project/Stored Procedures/dw.LoadDimEmployee_bak.sql
+++ LoadKingDW Solution/LoadKingDW DB Project/Stored Procedures/dw.LoadDimEmployee_bak.sql
@@ -1,4 +1,4 @@
-﻿CREATE PROCEDURE [dw].[sp_LoadDimEmployee] @LoadLogKey INT  AS
+﻿CREATE PROCEDURE [dw].[sp_LoadDimEmployee_bak] @LoadLogKey INT  AS
 
 BEGIN
 
@@ -50,12 +50,43 @@ BEGIN
 
 	--Load #work table with data in the format in which it will appear in the dimension
 	INSERT INTO #DimEmployee_work
-			SELECT 		
-				 * from dwstage.V_LoadDimEmployee
-    
-    Update #DimEmployee_work Set [DWEffectiveStartDate] = @CurrentTimestamp, [LoadLogKey]	 = @LoadLogKey
+	SELECT 		
+		  [EmployeeID]				= CAST(EMPLOYEE				AS NCHAR(5))
+		, [EmployeeRecordType]		= CAST(RECORD_TYPE			AS NCHAR(1))
+		, [EmployeeName]			= CAST([NAME]				AS NVARCHAR(100))
+		, [EmployeeAddress]			= CAST([ADDRESS]			AS NVARCHAR(100))
+		, [EmployeeCity]			= CAST(CITY					AS NVARCHAR(50))
+		, [EmployeeState]			= CAST([STATE]				AS NCHAR(2))	
+		, [EmployeePostalCode]		= CAST(ZIP_CODE				AS NCHAR(9))
+		, [EmployeeGender]			= CAST(SEX					AS NCHAR(1))
+		, [EmployeeHireDate]		= dwstage.udf_cv_nvarchar6_to_date(DATE_HIRE)
+		, [EmployeeTerminationDate]	= dwstage.udf_cv_nvarchar6_to_date(DATE_TERMINATION)
+		, [EmployeeDepartment]		= CAST(DEPT_EMPLOYEE		AS NCHAR(4))
+		, [EmployeeIsSalesperson]	= CAST(0					AS BIT) 	
+		, [EmployeeInitials]        = CAST(''                   AS NVARCHAR(3))
+		, [Type1RecordHash]			= CAST(0 AS VARBINARY(64))
+		, [Type2RecordHash]			= HASHBYTES('SHA2_256', CAST(EMPLOYEE		AS NCHAR(5))
+															+ CAST(RECORD_TYPE		AS NCHAR(1))
+															+ CAST([NAME]			AS NVARCHAR(100))
+															+ CAST([ADDRESS]		AS NVARCHAR(100))
+															+ CAST(CITY				AS NVARCHAR(50))
+															+ CAST([STATE]			AS NCHAR(2))	
+															+ CAST(ZIP_CODE			AS NCHAR(9))
+															+ CAST(SEX				AS NCHAR(1))
+															+ CAST(dwstage.udf_cv_nvarchar6_to_date(DATE_HIRE)  AS NVARCHAR(12))
+															+ CAST(dwstage.udf_cv_nvarchar6_to_date(DATE_TERMINATION)  AS NVARCHAR(12))
+															+ CAST(DEPT_EMPLOYEE	AS NCHAR(4))
+															+ CAST(0				AS NCHAR(1)))
+		
+		, [SourceSystemName]		= CAST('Global Shop' AS		NVARCHAR(100))
+		, [DWEffectiveStartDate]	= @CurrentTimestamp
+		, [DWEffectiveEndDate]		= '2100-01-01'
+		, [DWIsCurrent]				= CAST(1					AS BIT)
+		
+		, [LoadLogKey]				= @LoadLogKey
+	FROM  dwstage.EMPLOYEE_MSTR
 
-    ----  UPDATE The 
+
 	--CREATE TEMP table to be used below for identifying records with Type 2 changes
 	CREATE TABLE #DimEmployee_current (EmployeeID NCHAR(5)
 										, Type2RecordHash VARBINARY(64)
---
+++ LoadKingDW Solution/LoadKingDW DB Project/Views/dwstage.V_LoadDimEmployee.sql
@@ -0,0 +1,38 @@
+﻿Create VIEW [dwstage].[V_LoadDimEmployee]
+AS
+SELECT 		
+		  [EmployeeID]				= CAST(EMPLOYEE				AS NCHAR(5))
+		, [EmployeeRecordType]		= CAST(RECORD_TYPE			AS NCHAR(1))
+		, [EmployeeName]			= CAST([NAME]				AS NVARCHAR(100))
+		, [EmployeeAddress]			= CAST([ADDRESS]			AS NVARCHAR(100))
+		, [EmployeeCity]			= CAST(CITY					AS NVARCHAR(50))
+		, [EmployeeState]			= CAST([STATE]				AS NCHAR(2))	
+		, [EmployeePostalCode]		= CAST(ZIP_CODE				AS NCHAR(9))
+		, [EmployeeGender]			= CAST(SEX					AS NCHAR(1))
+		, [EmployeeHireDate]		= dwstage.udf_cv_nvarchar6_to_date(DATE_HIRE)
+		, [EmployeeTerminationDate]	= dwstage.udf_cv_nvarchar6_to_date(DATE_TERMINATION)
+		, [EmployeeDepartment]		= CAST(DEPT_EMPLOYEE		AS NCHAR(4))
+		, [EmployeeIsSalesperson]	= CAST(0					AS BIT) 	
+		, [EmployeeInitials]        = CAST(''                   AS NVARCHAR(3))
+		, [Type1RecordHash]			= CAST(0 AS VARBINARY(64))
+		, [Type2RecordHash]			= HASHBYTES('SHA2_256', CAST(EMPLOYEE		AS NCHAR(5))
+															+ CAST(RECORD_TYPE		AS NCHAR(1))
+															+ CAST([NAME]			AS NVARCHAR(100))
+															+ CAST([ADDRESS]		AS NVARCHAR(100))
+															+ CAST(CITY				AS NVARCHAR(50))
+															+ CAST([STATE]			AS NCHAR(2))	
+															+ CAST(ZIP_CODE			AS NCHAR(9))
+															+ CAST(SEX				AS NCHAR(1))
+															+ CAST(dwstage.udf_cv_nvarchar6_to_date(DATE_HIRE)  AS NVARCHAR(12))
+															+ CAST(dwstage.udf_cv_nvarchar6_to_date(DATE_TERMINATION)  AS NVARCHAR(12))
+															+ CAST(DEPT_EMPLOYEE	AS NCHAR(4))
+															+ CAST(0				AS NCHAR(1)))
+		
+		, [SourceSystemName]		= CAST('Global Shop'        AS NVARCHAR(100))
+		, [DWEffectiveStartDate]	= CAST(Getdate()            AS DATETIME2(7))
+		, [DWEffectiveEndDate]		= '2100-01-01'
+		, [DWIsCurrent]				= CAST(1					AS BIT)
+		
+		, [LoadLogKey]				= CAST(0                    AS INT)
+		FROM dwstage.EMPLOYEE_MSTR
+GO
