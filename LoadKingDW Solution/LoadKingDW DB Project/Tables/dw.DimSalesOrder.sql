0b5ce27d608fc01510d8984dfcdbe6d8441f757f
Bob Rubocki <brubocki@pragmaticworks.com>
2020-04-09 12:14:54 -0400

    Modifying and removing fields from DimSalesOrder.  Adding ShippedDate to FactSalesOrderLine.

--- LoadKingDW Solution/LoadKingDW DB Project/Tables/dw.DimSalesOrder.sql
+++ LoadKingDW Solution/LoadKingDW DB Project/Tables/dw.DimSalesOrder.sql
@@ -1,39 +1,22 @@
 ﻿CREATE TABLE [dw].[DimSalesOrder]
 (
-	DimSalesOrder_Key int identity(1,1) not null
-	, DimCustomer_Key int not null
-	, DimEmployee_Key int not null
-	, OrderNumber nvarchar(10) not null
-	, InvoiceNumber nvarchar(10) not null
-	, OrderDate date null
-	, DueDate date null
-	, OrderType nvarchar(1) null
-	, OrderSuffix nvarchar(4) null
-	, CustomerPO nvarchar(20) null
-	, FillCustomerPO nvarchar(20) null
-	, MarkInfo nvarchar(30) null
-	, CodeFOB nvarchar(14) null
-	, Terms nvarchar(10) null
-	, WayBill nvarchar(19) null
-	
-
+	DimSalesOrder_Key			INT IDENTITY(1,1) NOT NULL
+	, OrderNumber				NVARCHAR(10) NOT NULL	--Natural Key field
+	, OrderType					NVARCHAR(1) NULL
+	, OrderSuffix				NVARCHAR(4) NULL
+	, CustomerPO				NVARCHAR(20) NULL
 
 	/*Hashes used for identifying changes, not required for reporting*/
-	, Type1RecordHash				VARBINARY(64)				NULL	
-	, Type2RecordHash				VARBINARY(64)				NULL	
+	, Type1RecordHash			VARBINARY(64)				NULL	
+	, Type2RecordHash			VARBINARY(64)				NULL	
 
 	/*DW Metadata fields, not required for reporting*/
 	, SourceSystemName			NVARCHAR(100)		NOT NULL
 	, DWEffectiveStartDate		DATETIME2(7)		NOT NULL
-	, DWEffectiveEndDate			DATETIME2(7)		NOT NULL
-	, DWIsCurrent					BIT					NOT NULL
+	, DWEffectiveEndDate		DATETIME2(7)		NOT NULL
+	, DWIsCurrent				BIT					NOT NULL
 
-	/*ETL Metadata fields, not required for reporting (DWEffectiveStartDate may not neccessarily be the same as RecordCreateDate, for example 
-	 Commented fields not required for this project due to use of ETL control table*/
-	--RecordCreateDate			DATETIME2(7)		NOT NULL,
- --   RecordLastUpdatedDate		DATETIME2(7)		NOT NULL,
- --   RecordCreatedByName			NVARCHAR (100)		NOT NULL,
- --   RecordLastUpdatedByName		NVARCHAR (100)		NOT NULL,
-	, LoadLogKey					INT					NOT NULL --ID of ETL process that inserted the record
+	/*ETL Metadata fields, not required for reporting (DWEffectiveStartDate may not neccessarily be the same as RecordCreateDate, for example */
+	, LoadLogKey				INT					NOT NULL --ID of ETL process that inserted the record
 	, constraint pk_DimSalesOrder primary key clustered (DimSalesOrder_Key)
 )
--- LoadKingDW Solution/LoadKingDW DB Project/Tables/dw.FactSalesOrderLine.sql
+++ LoadKingDW Solution/LoadKingDW DB Project/Tables/dw.FactSalesOrderLine.sql
@@ -1,48 +1,53 @@
 ﻿CREATE TABLE [dw].[FactSalesOrderLine]
 (
 	
-	FactSalesOrderLine_Key int identity(1,1) not null
+	FactSalesOrderLine_Key			INT IDENTITY(1,1)	NOT NULL
 	-- dimensions
-	, DimSalesOrder_Key int not null
-	, DimCustomer_Key int not null
-	, OrderDimDate_Key int not null
-	, DueDimDate_Key int not null
-	, DimProduct_Key int not null
-	, DimPart_Key int not null
-	, DimEmployee_Key int not null
-	, DimCustomerShipTo_Key int not null
-	, DimOrderType_Key int not null
+	, OrderNumber					NVARCHAR(10)		NOT NULL		--UNIQUE KEY
+	, OrderLineNumber				NVARCHAR(4)			NOT NULL--UNIQUE KEY
+
+	, DimSalesOrder_Key				INT					NOT NULL
+	, DimCustomer_Key				INT					NOT NULL
+	, OrderDimDate_Key				INT					NOT NULL
+	, DueDimDate_Key				INT					NOT NULL
+	, ShippedDate_Key				INT					NOT NULL
+
+	, DimProduct_Key				INT					NOT NULL
+	, DimPart_Key					INT					NOT NULL
+	, DimEmployee_Key				INT					NOT NULL
+	, DimCustomerShipTo_Key			INT					NOT NULL
+	, DimOrderType_Key				INT					NOT NULL
 	
 	-- measures
-	, QuantityOrdered decimal (13,4) -- should quantity be decimal??
-	, Cost decimal (16,4) 
-	, MaterialCost decimal (16,4)
-	, LaborCost decimal (16,4)
-	, OutsideCost decimal (16,4)
-	, OverheadCost decimal (16,4)
-	, OtherCost decimal(16,4)
-	, Margin decimal(16,4)
-	, Price decimal(16,4)
-	, PriceDiscount decimal(16,4)
-	, PricePerPound decimal(16,4)
-	, DiscountAmount decimal(16,4)
-	, OrderDiscount decimal(5,4)
-	, PriceClassDiscount decimal(5,4) 
-	, ProductLineDiscount decimal(5,4)
-	, OrderDiscountAmount decimal(16,4)
-	, ProductClassDiscountAmount decimal(16,4)
-	, ProductLineDiscountAmount decimal(16,4)
-	, OrderPrice decimal(16,4)
-	, OrderDiscountPrice decimal(16,4)
-	, OrderPricePerPound decimal(16,4)
+	, QuantityOrdered				DECIMAL (13,4) -- Should quantity be decimal??
+	, Cost							DECIMAL (16,4) 
+	, MaterialCost					DECIMAL (16,4)
+	, LaborCost						DECIMAL (16,4)
+	, OutsideCost					DECIMAL (16,4)
+	, OverheadCost					DECIMAL (16,4)
+	, OtherCost						DECIMAL(16,4)
+	, Margin						DECIMAL(16,4)
+	, Price							DECIMAL(16,4)
+	, PriceDiscount					DECIMAL(16,4)
+	, PricePerPound					DECIMAL(16,4)
+	, DiscountAmount				DECIMAL(16,4)
+	, OrderDiscount					DECIMAL(5,4)
+	, PriceClassDiscount			DECIMAL(5,4) 
+	, ProductLineDiscount			DECIMAL(5,4)
+	, OrderDiscountAmount			DECIMAL(16,4)
+	, ProductClassDiscountAmount	DECIMAL(16,4)
+	, ProductLineDiscountAmount		DECIMAL(16,4)
+	, OrderPrice					DECIMAL(16,4)
+	, OrderDiscountPrice			DECIMAL(16,4)
+	, OrderPricePerPound			DECIMAL(16,4)
 
 	/*Hash used for identifying changes, not required for reporting*/
-	,RecordHash					VARBINARY(64)			NULL
+	,RecordHash						VARBINARY(64)			NULL
 
 	/*DW Metadata fields, not required for reporting*/
-	,SourceSystemName			NVARCHAR(100)		NOT NULL
+	,SourceSystemName				NVARCHAR(100)		NOT NULL
 
 	/*ETL Metadata fields, not required for reporting */
-	,LoadLogKey					INT					NOT NULL	--ID of ETL process that inserted the record
+	,LoadLogKey						INT					NOT NULL	--ID of ETL process that inserted the record
 	, constraint pk_FactSalesOrderLine primary key nonclustered (FactSalesOrderLine_Key)
 )
