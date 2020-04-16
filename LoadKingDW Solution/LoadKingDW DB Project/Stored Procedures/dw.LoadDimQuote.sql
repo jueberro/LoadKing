63d0eb1707aff0d8ad1055115802e7d343bbb047
MogenisMax <54866150+MogenisMax@users.noreply.github.com>
2020-04-16 16:03:26 -0400

    Max created view/sproc and dtsx package to load quote information, also updated the entities around them --416

---
+++ LoadKingDW Solution/LoadKingDW DB Project/Stored Procedures/dw.LoadDimQuote.sql
@@ -0,0 +1,98 @@
+﻿CREATE PROCEDURE [dw].[sp_LoadDimQuote] @LoadLogKey INT  AS
+
+BEGIN
+
+
+	/*
+
+	- This procedure is called from an SSIS package
+	- This procedure assumes that a stage table has been loaded with data for one and ONLY one batch of source data
+	
+	- @LoadLogKey	- corresponds to LoadLogKey in dwetl.LoadLog table
+
+	*/
+
+	DECLARE		@CurrentTimestamp DATETIME2(7)
+
+	SELECT		@CurrentTimestamp = GETUTCDATE()
+
+	BEGIN TRY DROP TABLE #DimQuote_work		END TRY BEGIN CATCH END CATCH
+	BEGIN TRY DROP TABLE #DimQuote_current	END TRY BEGIN CATCH END CATCH
+
+	--CREATE TEMP table With SAME structure as destination table (except for IDENTITY field)
+	CREATE TABLE #DimQuote_work (
+		[QuoteNumber]				[nchar](7) NOT NULL,
+		[QuoteCreationDate]			[nchar](7) NULL,
+		[QuoteWonLossDate]			[nchar](6) NULL,
+
+
+		/*Hashes used for identifying changes, not required for reporting*/
+		[Type1RecordHash]			VARBINARY(64)  	NULL,	--66 allows for "0x" + 64 characater hash
+		[Type2RecordHash]			VARBINARY(64)	NULL,	--66 allows for "0x" + 64 characater hash
+
+		/*DW Metadata fields, not required for reporting*/
+		[SourceSystemName]			NVARCHAR(100)		NOT NULL,
+		[DWEffectiveStartDate]		DATETIME2(7)		NOT NULL,
+		[DWEffectiveEndDate]		DATETIME2(7)		NOT NULL,
+		[DWIsCurrent]				BIT					NOT NULL,
+
+		/*ETL Metadata fields, not required for reporting (DWEffectiveStartDate may not neccessarily be the same as RecordCreateDate, for example */
+		[LoadLogKey]				INT
+	)
+
+	--Load #work table with data in the format in which it will appear in the dimension
+	INSERT INTO #DimQuote_work
+			SELECT 		
+				 * from dwstage.V_LoadDimQuote
+    
+    Update #DimQuote_work Set [DWEffectiveStartDate] = @CurrentTimestamp, [LoadLogKey]	 = @LoadLogKey
+
+    ----  UPDATE The 
+	--CREATE TEMP table to be used below for identifying records with Type 2 changes
+	CREATE TABLE #DimQuote_current (QuoteNumber NVARCHAR(6)
+										, Type2RecordHash VARBINARY(64)
+										)
+
+	--Load temp table with NK and Type2RecordHash for CURRENT dimension records
+	INSERT INTO #DimQuote_current
+	SELECT	QuoteNumber
+			, Type2RecordHash
+	FROM	dw.DimQuote
+	WHERE	DWIsCurrent = 1
+
+
+	--INSERT NEW Dimension Items
+	INSERT INTO dw.DimQuote 
+	SELECT	*
+	FROM	#DimQuote_work AS Work
+	WHERE	NOT EXISTS(	SELECT  1
+						FROM	dw.DimQuote AS DIM
+						WHERE	DIM.QuoteNumber = Work.QuoteNumber 
+						)
+
+
+	--UPDATE/Expire Existing Items that have Type 2 changes
+	UPDATE	DIM
+	SET		DWEffectiveEndDate = @CurrentTimestamp
+			, DWIsCurrent = 0
+	FROM	dw.DimQuote		AS DIM
+	 JOIN   #DimQuote_work	AS Work
+	  ON	Dim.QuoteNumber  = Work.QuoteNumber 
+	   AND	Dim.DWIsCurrent = 1
+	WHERE	DIM.Type2RecordHash <> Work.Type2RecordHash
+
+
+	--INSERT New versions of expired records that have Type 2 changes
+	INSERT INTO dw.DimSalesPerson
+	SELECT	Work.*
+	FROM	#DimQuote_current AS DIM
+	 JOIN   #DimQuote_work    AS Work
+	  ON	Dim.QuoteNumber = Work.QuoteNumber
+	WHERE	DIM.Type2RecordHash <> Work.Type2RecordHash
+	
+	--DROP temp tables
+	BEGIN TRY DROP TABLE #DimQuote_work		END TRY BEGIN CATCH END CATCH
+	BEGIN TRY DROP TABLE #DimQuote_current	END TRY BEGIN CATCH END CATCH
+	 
+
+END
--- LoadKingDW Solution/LoadKingDW DB Project/Tables/dwstage.Quote.sql
+++ LoadKingDW Solution/LoadKingDW DB Project/Tables/dwstage.Quote.sql
@@ -1,10 +1,9 @@
-﻿CREATE TABLE [dw].[DimQuote]
-(
-	[DimQuote_Key] int IDENTITY(1,1),
-	[QuoteNumber] [nchar](7) NULL,
-	[QuoteCreationDate] CHAR(6) NULL,
-	[QuoteWonLostDate] CHAR(8) NULL,
+﻿CREATE TABLE [dwstage].[DimQuote] (
 
+    [QuoteNo]                   NVARCHAR(7)      NOT NULL,
+    [DATE_QUOTE]                [char](6) NULL,
+    [QUOTE_WON_LOSS_DATE]       [char](8) NULL,
+   
 	/*Hashes used for identifying changes, not required for reporting*/
 	Type1RecordHash				VARBINARY(64)				NULL,
 	Type2RecordHash				VARBINARY(64)				NULL,
@@ -18,4 +17,5 @@
 	/*ETL Metadata fields, not required for reporting (DWEffectiveStartDate may not neccessarily be the same as RecordCreateDate, for example */
 	LoadLogKey					INT					NOT NULL, --ID of ETL process that inserted the record    CONSTRAINT [pk_DimEmployee] PRIMARY KEY CLUSTERED ([DimEmployee_Key] ASC)
 
-    CONSTRAINT [pk_DimQuote] PRIMARY KEY CLUSTERED ([DimQuote_Key] ASC))
+    
+);
