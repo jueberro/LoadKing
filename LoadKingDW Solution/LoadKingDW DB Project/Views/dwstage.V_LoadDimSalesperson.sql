1d251e21468bc35cf88e7f280a9b35f14436da51
MogenisMax <54866150+MogenisMax@users.noreply.github.com>
2020-04-16 10:13:17 -0400

    Max's first attempts at creating the view and stored procedure for the SalespersonDim --41620

---
+++ LoadKingDW Solution/LoadKingDW DB Project/Views/dwstage.V_LoadDimSalesperson.sql
@@ -0,0 +1,29 @@
+﻿CREATE VIEW [dwstage].[V_LoadDimSalesperson]
+AS
+SELECT 		                                 
+		  [SalespersonID]	  = CAST([SalespersonID]	AS NVARCHAR(3))
+		, [Name]			  = CAST([NAME]				AS NVARCHAR(100))
+		, [EMAIL]			  = CAST([EMAIL]			AS NVARCHAR(100))
+
+		, [Type1RecordHash]		      = CAST(0 AS VARBINARY(64))
+		, [Type2RecordHash]			  = HASHBYTES('SHA2_256',        
+															+ CAST([ID]				AS NVARCHAR(3))
+															+ CAST([NAME]	        AS NVARCHAR(100))
+															+ CAST([EMAIL]			AS NVARCHAR(100))
+ )
+	
+	
+	
+	    , [SourceSystemName]		  = CAST('Global Shop'        AS NVARCHAR(100))
+		, [DWEffectiveStartDate]	  = CAST(Getdate()            AS DATETIME2(7))
+		, [DWEffectiveEndDate]		  = '2100-01-01'
+		, [DWIsCurrent]				  = CAST(1					  AS BIT)
+		, [LoadLogKey]				  = CAST(0                    AS INT)
+
+
+
+		FROM     dwstage.SALESPERSONS
+		WHERE    dwstage.SALESPERSONS.REC = 1  
+GO
+
+
