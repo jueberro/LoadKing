c2544d22d6c60ff2d907c791401292134ea77845
MogenisMax <54866150+MogenisMax@users.noreply.github.com>
2020-04-16 17:44:58 -0400

    Max updated entity, view, and dts package per Bob's feedback at eod. Max will slow down and follow the pattern so as to avoid introducing unforced errors.

--- LoadKingDW Solution/LoadKingDW DB Project/Views/dwstage.V_LoadDimQuote.sql
+++ LoadKingDW Solution/LoadKingDW DB Project/Views/dwstage.V_LoadDimQuote.sql
@@ -1,13 +1,13 @@
 ﻿CREATE VIEW [dwstage].[V_LoadDimQuote]
 AS
 SELECT 		                                 
-		  [QuoteNumber]					= CAST([QuoteNo]	AS CHAR(7))
+		  [QuoteNumber]					= CAST([QUOTE_NO]	AS CHAR(7))
 		, [QuoteCreationDate]			= CAST([DATE_QUOTE]	AS char(6))
 		, [QuoteWonLossDate]			= CAST([QUOTE_WON_LOSS_DATE] AS CHAR(8))
 		
 		, [Type1RecordHash]				= CAST(0 AS VARBINARY(64))
 		, [Type2RecordHash]				= HASHBYTES('SHA2_256',        
-															+ CAST([QuoteNo]				AS CHAR(7))
+															+ CAST([Quote_No]				AS CHAR(7))
 															+ CAST([DATE_QUOTE]	        AS CHAR(6))
 															+ CAST([QUOTE_WON_LOSS_DATE]			AS CHAR(8))
  )
@@ -22,7 +22,7 @@ SELECT
 
 
 
-		FROM     dwstage.DimQuote
+		FROM     dwstage.QUOTE_HEADER
 		
 GO
 
