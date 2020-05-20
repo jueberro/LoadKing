6b65fd5a2ca58691da496afe2c486447162e3e86
Edwin Davis <v-eddavi@microsoft.com>
2020-05-20 16:04:50 -0400

    updated SSIS pkgs for DimEmployee and DimSalesPerson.  Added dwstage tables for Quality sources

---
+++ LoadKingDW Solution/LoadKingDW DB Project/Stored Procedures/dw.LoadFactQuality.sql
@@ -0,0 +1,194 @@
+--USE [LK-GS-EDW]
+--GO
+
+CREATE PROCEDURE dw.sp_LoadFactQuality @LoadLogKey INT  AS
+
+BEGIN
+
+--DECLARE @LoadLogKey int
+--SET @LoadLogKey = 0
+
+DECLARE @RowsInsertedCount int
+DECLARE @RowsUpdatedCount int
+
+	/*
+	- This procedure is called from an SSIS package
+	- This procedure assumes that a stage table has been loaded with data for one and ONLY one batch of source data
+	
+	- @LoadLogKey	- corresponds to LoadLogKey in dwetl.LoadLog table
+	*/
+
+	DECLARE		@CurrentTimestamp DATETIME2(7)
+
+	SELECT		@CurrentTimestamp = GETUTCDATE()
+
+IF object_id('##FactQuality_SOURCE', 'U') is not null -- if table exists
+	BEGIN
+		Drop table ##FactQuality_SOURCE
+	END
+
+IF object_id('##FactQuality_TARGET', 'U') is not null -- if table exists
+	BEGIN
+		Drop table ##FactQuality_TARGET
+	END
+
+	--CREATE TEMP table With SAME structure as destination table (except for IDENTITY field)
+	CREATE TABLE ##FactQuality_SOURCE (
+
+DimSalesOrder_Key int NOT NULL,
+DimWorkOrderType_Key int NOT NULL,
+DimInventory_Key int NOT NULL,
+DimCustomer_Key int NOT NULL,
+DimSalesPerson_Key int NOT NULL,
+DimProductLine_Key int NOT NULL,
+DimDate_Key int NOT NULL,
+-- DEGENERATE DETAIL ATTRIBUTES ----------------------------------------------
+[JOB] [varchar](50) NULL,
+[SUFFIX] [varchar](50) NULL,
+[SEQ] [varchar](50) NULL,
+
+-- DATES ----------------------------------------------------
+
+
+-- OPERATIONS MEASURES ------------------------------------------------------
+
+
+/*Hashes used for identifying changes, not required for reporting*/
+[Type1RecordHash]			VARBINARY(64)	NULL	--66 allows for "0x" + 64 characater hash
+)
+
+	--Load #SOURCE table with data in the format in which it will appear in the dimension
+	INSERT INTO ##FactQuality_SOURCE
+			SELECT * from dwstage.V_LoadFactQuality
+
+--CREATE TEMP table to be used below for identifying records with CHANGES 
+
+	CREATE TABLE ##FactQuality_TARGET 
+					(
+					JOB CHAR(6)
+					,SUFFIX CHAR(3)
+					,SEQ CHAR(6)
+					,PART CHAR(50)
+					,HEADER_CUSTOMER CHAR(6)
+					, Type1RecordHash VARBINARY(64)
+					)
+
+	--Load temp table with NK and Type1RecordHash for CURRENT records
+	INSERT INTO ##FactQuality_TARGET
+	SELECT	
+			JOB
+			,SUFFIX
+			,SEQ
+			,PART
+			,HEADER_CUSTOMER
+			, Type1RecordHash
+	FROM	dw.FactQuality
+
+	--INSERT NEW TARGET Items
+	INSERT INTO dw.FactQuality 
+	SELECT	*
+	FROM	##FactQuality_SOURCE AS SRC
+	WHERE	NOT EXISTS(	SELECT  1
+						FROM	dw.FactQuality AS TGT
+						WHERE	
+							TGT.JOB = SRC.JOB
+							and TGT.SUFFIX = SRC.SUFFIX
+							and TGT.SEQ = SRC.SEQ
+							and TGT.PART = SRC.PART
+							and TGT.HEADER_CUSTOMER = SRC.HEADER_CUSTOMER					
+						)
+
+SET @RowsInsertedCount = @@ROWCOUNT
+
+	--UPDATE Existing Items that have CHANGES
+
+	UPDATE	TGT
+	SET
+	TGT.DimSalesOrder_Key = SRC.DimSalesOrder_Key
+	,TGT.DimWorkOrderType_Key = SRC.DimWorkOrderType_Key
+	,TGT.DimInventory_Key = SRC.DimInventory_Key
+	,TGT.DimCustomer_Key = SRC.DimCustomer_Key
+	,TGT.DimSalesperson_Key = SRC.DimSalesPerson_Key
+	,TGT.DimProductLine_Key = SRC.DimProductLine_Key
+	,TGT.DimDate_Key = SRC.DimDate_Key
+	-----------------------------------
+	, TGT.[HEADER_JOB] = SRC.[HEADER_JOB]
+	, TGT.[HEADER_SUFFIX] = SRC.[HEADER_SUFFIX]
+	, TGT.[HEADER_PART] = SRC.[HEADER_PART]
+	, TGT.[HEADER_PRODUCT_LINE] = SRC.[HEADER_PRODUCT_LINE]
+	, TGT.HEADER_SALESPERSON = SRC.HEADER_SALESPERSON
+	, TGT.[HEADER_SALES_ORDER] = SRC.[HEADER_SALES_ORDER]
+	, TGT.[HEADER_SALES_ORDER_LINE] = SRC.[HEADER_SALES_ORDER_LINE]
+	, TGT.[PROJ_GROUP] = SRC.[PROJ_GROUP]
+	, TGT.[OPERATION] = SRC.[OPERATION]
+	, TGT.[LMO] = SRC.[LMO]
+	, TGT.[DESCRIPTION] = SRC.[DESCRIPTION]
+	, TGT.[UM] = SRC.[UM]
+	, TGT.[PART] = SRC.[PART]
+	, TGT.[ROUTER] = SRC.[ROUTER]
+	, TGT.[ROUTER_SEQ] = SRC.[ROUTER_SEQ]
+	, TGT.[FLAG_CLOSED] = SRC.[FLAG_CLOSED]
+	, TGT.[TIME_START] = SRC.[TIME_START]
+	, TGT.[MACHINE_HRS] = SRC.[MACHINE_HRS]
+	, TGT.[TIME_ELAPSED] = SRC.[TIME_ELAPSED]
+	, TGT.[FACTOR_WORKCENTER] = SRC.[FACTOR_WORKCENTER]
+	, TGT.[SEQ_PO] = SRC.[SEQ_PO]
+	, TGT.[PO_ASSIGNED] = SRC.[PO_ASSIGNED]
+	, TGT.[MAIN_COMMENT] = SRC.[MAIN_COMMENT]
+	, TGT.[WORK_STARTED] = SRC.[WORK_STARTED]
+	, TGT.[HOLD_PO] = SRC.[HOLD_PO]
+	----------------------------------
+	, TGT.[HEADER_DATE_OPENED] = SRC.[HEADER_DATE_OPENED]
+	, TGT.[HEADER_DATE_DUE] = SRC.[HEADER_DATE_DUE]
+	, TGT.[HEADER_DATE_CLOSED] = SRC.[HEADER_DATE_CLOSED]
+	, TGT.[HEADER_DATE_START] = SRC.[HEADER_DATE_START]
+	, TGT.[DATE_START] = SRC.[DATE_START]
+	, TGT.[DATE_DUE] = SRC.[DATE_DUE]
+	, TGT.[DATE_MATERIAL_DUE] = SRC.[DATE_MATERIAL_DUE]
+	, TGT.[DATE_COMPLETED] = SRC.[DATE_COMPLETED]
+	, TGT.[DATE_HARD] = SRC.[DATE_HARD]
+	, TGT.[DATE_OPER_ST_MDY] = SRC.[DATE_OPER_ST_MDY]
+	, TGT.[DATE_PO_ORDER] = SRC.[DATE_PO_ORDER]
+	, TGT.[DATE_OPER_SK_YEAR] = SRC.[DATE_OPER_SK_YEAR]
+	, TGT.[DATE_OPER_SK_MDY] = SRC.[DATE_OPER_SK_MDY]
+	, TGT.[DATE_OPER_ST_YEAR] = SRC.[DATE_OPER_ST_YEAR]
+	--------------------------------
+	, TGT.[UNITS_OPEN] = SRC.[UNITS_OPEN]
+	, TGT.[UNITS_COMPLETE] = SRC.[UNITS_COMPLETE]
+	, TGT.[SETUP] = SRC.[SETUP]
+	, TGT.[UNITS] = SRC.[UNITS]
+	, TGT.[BURDEN] = SRC.[BURDEN]
+	, TGT.[HOURS_ESTIMATED] = SRC.[HOURS_ESTIMATED]
+	, TGT.[HOURS_ACTUAL] = SRC.[HOURS_ACTUAL]
+	, TGT.[DOLLARS_ESTIMATED] = SRC.[DOLLARS_ESTIMATED]
+	, TGT.[DOLLARS_ACTUAL] = SRC.[DOLLARS_ACTUAL]
+	, TGT.[YIELD] = SRC.[YIELD]
+	, TGT.[YIELD_RATIO] = SRC.[YIELD_RATIO]
+	, TGT.[CREW_SIZE] = SRC.[CREW_SIZE]
+	, TGT.[UNIT_D6] = SRC.[UNIT_D6]
+	, TGT.[LEAD_TIME] = SRC.[LEAD_TIME]
+	, TGT.Type1RecordHash = SRC.Type1RecordHash
+	FROM	dw.FactQuality		AS TGT
+	 JOIN   ##FactQuality_SOURCE	AS SRC
+			ON TGT.JOB = SRC.JOB
+			and TGT.SUFFIX = SRC.SUFFIX
+			and TGT.SEQ = SRC.SEQ
+			and TGT.PART = SRC.PART
+			and TGT.HEADER_CUSTOMER = SRC.HEADER_CUSTOMER					
+	WHERE	TGT.Type1RecordHash <> SRC.Type1RecordHash
+	
+	--DROP temp tables
+
+SET @RowsUpdatedCount = @@ROWCOUNT
+
+	 DROP TABLE ##FactQuality_SOURCE		
+	 DROP TABLE ##FactQuality_TARGET	
+	 
+END
+
+SELECT RowsInsertedCount = @RowsInsertedCount, RowsUpdatedCount = @RowsUpdatedCount
+
+
+GO
+
+
