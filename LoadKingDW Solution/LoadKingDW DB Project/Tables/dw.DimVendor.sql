ab726f01cca56f73b5c718deaad95c7e0b74f2e2
Joe Ueberroth <jueberroth@pragmaticworks.com>
2020-06-03 08:37:58 -0400

    commit changes

---
+++ LoadKingDW Solution/LoadKingDW DB Project/Tables/dw.DimVendor.sql
@@ -0,0 +1,30 @@
+﻿CREATE TABLE [dw].[DimVendor](
+	[DimVendor_Key] [int] IDENTITY(1,1) NOT NULL,
+	[Vendor] [nchar](6) NOT NULL,
+	[VendorName] [nvarchar](30) NOT NULL,
+	[VendorAddress1] [nvarchar](30) NULL,
+	[VendorAddress2] [nvarchar](30) NULL,
+	[VendorCity] [nvarchar](20) NULL,
+	[VendorState] [nchar](2) NULL,
+	[VendorPostalCode] [nchar](13) NULL,
+	[VendorCountry] [nchar](12) NULL,
+	[VendorCounty] [nchar](12) NULL,
+	[VendorIntlFlag] [nchar](1) NULL,
+	[VendorTerritory] [nchar](2) NULL,
+	[VendorCodeArea] [nchar](2) NULL,
+	[VendorEmail] [nvarchar](30) NULL,
+	[Type1RecordHash] [varbinary](64) NULL,
+	[Type2RecordHash] [varbinary](64) NULL,
+	[SourceSystemName] [nvarchar](100) NOT NULL,
+	[DWEffectiveStartDate] [datetime2](7) NOT NULL,
+	[DWEffectiveEndDate] [datetime2](7) NOT NULL,
+	[DWIsCurrent] [bit] NOT NULL,
+	[LoadLogKey] [int] NOT NULL,
+ CONSTRAINT [pk_DimVendor] PRIMARY KEY CLUSTERED 
+(
+	[DimVendor_Key] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+
---
+++ LoadKingDW Solution/LoadKingDW DB Project/Views/dwstage.V_LoadDimVendor.sql
@@ -0,0 +1,47 @@
+﻿CREATE VIEW [dwstage].[V_LoadDimVendor]
+AS
+SELECT 		                                 
+		    [Vendor]            = CAST(vm.[Vendor]         AS [nchar](6))
+	      , [VendorName]        = CAST(vm.[NAME_VENDOR]    AS [nvarchar](30))
+	      , [VendorAddress1]    = CAST(vm.[Address1]       AS [nvarchar](30))
+	      , [VendorAddress2]    = CAST(vm.[Address2]       AS [nvarchar](30))
+	      , [VendorCity]        = CAST(vm.[City]           AS [nvarchar](20))
+	      , [VendorState]       = CAST(vm.[State]          AS [nchar](2))
+	      , [VendorPostalCode]  = CAST(vm.[Code_Zip]       AS [nchar](13))
+	      , [VendorCountry]     = CAST(vm.[Country]        AS [nchar](12))
+	      , [VendorCounty]      = CAST(vm.[County]         AS [nchar](12))
+	      , [VendorIntlFlag]    = CAST(vm.[INTL_ADRS]      AS [nchar](1))
+	      , [VendorTerritory]   = CAST(vm.[Territory]      AS [nchar](2))
+	      , [VendorCodeArea]    = CAST(vm.[Code_Area]      AS [nchar](2))
+		  , [VendorEmail]       = CAST(vm2.[Address2]   AS [nvarchar](30))
+		  , [Type1RecordHash]		      = CAST(0 AS VARBINARY(64))
+		  , [Type2RecordHash]			  = HASHBYTES('SHA2_256',        
+															+ CAST(vm.[Vendor]         AS [nchar](6))
+															+ CAST(vm.[NAME_VENDOR]    AS [nvarchar](30))
+															+ CAST(vm.[Address1]       AS [nvarchar](30))
+															+ CAST(vm.[Address2]       AS [nvarchar](30))
+															+ CAST(vm.[City]           AS [nvarchar](20))
+															+ CAST(vm.[State]          AS [nchar](2))
+															+ CAST(vm.[Code_Zip]       AS [nchar](13))
+															+ CAST(vm.[Country]        AS [nchar](12))
+															+ CAST(vm.[County]         AS [nchar](12))
+															+ CAST(vm.[INTL_ADRS]      AS [nchar](1))
+															+ CAST(vm.[Territory]      AS [nchar](2))
+															+ CAST(vm.[Code_Area]      AS [nchar](2))
+															+ CAST(vm2.[Address2]   AS [nvarchar](30))
+															)
+	
+	
+	
+	     , [SourceSystemName]		  = CAST('Global Shop'        AS NVARCHAR(100))
+		 , [DWEffectiveStartDate]	  = CAST(Getdate()            AS DATETIME2(7))
+		 , [DWEffectiveEndDate]		  = '2100-01-01'
+		 , [DWIsCurrent]			  = CAST(1					  AS BIT)
+		 , [LoadLogKey]				  = CAST(0                    AS INT)
+
+
+
+		FROM         (Select * from dwstage.VENDOR_MASTER WHERE   dwstage.VENDOR_MASTER.REC = 1) as vm
+		INNER JOIN   (Select * from dwstage.VENDOR_MASTER WHERE   dwstage.VENDOR_MASTER.REC = 3) as vm2
+		ON vm.Vendor = vm2.Vendor
+GO
