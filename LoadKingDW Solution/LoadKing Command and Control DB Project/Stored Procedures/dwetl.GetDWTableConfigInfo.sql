6d2a25acff7cc006cbd1c1105507ba482cbbe28f
Joe Ueberroth <jueberroth@pragmaticworks.com>
2020-06-09 13:29:48 -0400

    Added new control table called dwetl.DWTableSourceSort to Sort the dwetl.DWTableSource to allow LoadDimWorkOrder to run befor LoadDimProductline to populate _V_JOB_HEADER.  Modified SSISPackage DWLoadDimWorkOrder to use _V_JOB_HEADER.  Modified View V_LoadDimProductLine to refernce _V_JOB_HEADER.

--- LoadKingDW Solution/LoadKing Command and Control DB Project/Stored Procedures/dwetl.GetDWTableConfigInfo.sql
+++ LoadKingDW Solution/LoadKing Command and Control DB Project/Stored Procedures/dwetl.GetDWTableConfigInfo.sql
@@ -17,12 +17,16 @@ BEGIN
 		- @TableType - Dimension, Fact, Lookup
 	*/
 
-	SELECT  DWTableName
-		  , SourceSystemName
-		  , SourceDataSetName
-		  , SSISPackageName
-	FROM	dwetl.DWTableSource
-	WHERE	DWTableType = @DWTableType
+	SELECT  ds.DWTableName
+		  , ds.SourceSystemName
+		  , ds.SourceDataSetName
+		  , ds.SSISPackageName
+	FROM	dwetl.DWTableSource ds
+	LEFT JOIN dwetl.DWTableSourceSort dsd
+	ON  ds.DWTableName = dsd.DWTableName
+	WHERE	ds.DWTableType = @DWTableType
 	 AND	LoadDWTableFlag = 1			-- Used primarily during testing to enable/disable loads
+	Order By dsd.Sort
 
 END
+
