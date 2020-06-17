d89d27349bbc27cd14eb99a9826deb8662acd4bf
Max Granados <mgranados@pragmaticworks.com>
2020-06-17 11:29:16 -0400

    Added ODS Create Table scripts

---
+++ LoadKingDW Solution/LoadKingODS DB Project/Tables/dbo.DEPARTMENTS.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[DEPARTMENTS](
+	[DEPT_ID] [char](4) NULL,
+	[DEPT_NAME] [char](50) NULL,
+	[FILLER] [char](100) NULL,
+	[LAST_DATE_CHG] [char](8) NULL,
+	[LAST_TIME_CHG] [char](8) NULL,
+	[LAST_USER_CHG] [char](8) NULL,
+	[LAST_PRGM_CHG] [char](8) NULL,
+	[ETL_TblNbr] [int] NULL,
+	[ETL_Batch] [int] NULL,
+	[ETL_Completed] [datetime] NULL
+) ON [PRIMARY]
