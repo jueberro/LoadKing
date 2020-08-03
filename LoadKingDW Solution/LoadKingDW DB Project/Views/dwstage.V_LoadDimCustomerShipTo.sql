96d21b46b8e9ec7b84b608dda19f5db2c7a471df
Joe Ueberroth <jueberroth@pragmaticworks.com>
2020-08-03 11:08:44 -0400

    update V_LoadDimCustomerShipTo to fix the date conversion on Last_Date_CHG.  Modified SSIS package DWLoadDimCustomerShipTo.

--- LoadKingDW Solution/LoadKingDW DB Project/Views/dwstage.V_LoadDimCustomerShipTo.sql
+++ LoadKingDW Solution/LoadKingDW DB Project/Views/dwstage.V_LoadDimCustomerShipTo.sql
@@ -27,20 +27,16 @@
                 ,   ShipToTax5              = CAST(cs.SHIP_TAX_5         AS NCHAR(3)    )
                 ,   ShipToWhoLastChanged    = CAST(cs.WHO_LAST_CHG       AS NVARCHAR(8) )
                 ,   ShipToTrmLastChanged    = CAST(cs.TRM_LAST_CHG       AS NUMERIC(2,0))
-                ,   ShipToDateLastChanged   = dwstage.udf_cv_char6_date_char8_time_to_datetime(
-                                               cs.DATE_LAST_CHG      
-                                              ,cs.TIME_LAST_CHG      
-                                              )
-                ,   ShipToPrimaryGroup      = CAST(cs.PRIMARY_GRP        AS NCHAR(2)    )
+                ,   ShipToDateLastChanged   = dwstage.udf_cv_nvarchar6__yymmdd_to_date(cs.DATE_LAST_CHG)
+				,   ShipToPrimaryGroup      = CAST(cs.PRIMARY_GRP        AS NCHAR(2)    )
                 ,   ShipToCarrierCD         = CAST(cs.CARRIER_CD         AS NCHAR(6)    )
                 ,   [Type1RecordHash]		      = CAST(0 AS VARBINARY(64))
 		        ,   [Type2RecordHash]			  = HASHBYTES('SHA2_256',        
 		        	    												+ CAST([CUSTOMER]				AS NCHAR(6))
 		        	    												+ CAST([SHIP_SEQ]	            AS NCHAR(6))
-		        	    												+ CAST(dwstage.udf_cv_char6_date_char8_time_to_datetime(
+		        	    												+ CAST(dwstage.udf_cv_nvarchar6__yymmdd_to_date(
                                                                                         cs.DATE_LAST_CHG      
-                                                                                       ,cs.TIME_LAST_CHG      
-                                                                              ) 
+                                                                                       ) 
                                                                               AS NVARCHAR(24)))
 	
 	
@@ -50,3 +46,4 @@
 		        ,   [DWIsCurrent]				  = CAST(1					  AS BIT)
 		        ,   [LoadLogKey]				  = CAST(0                    AS INT)
 FROM            dwstage.OE_MULTI_SHIP cs
+GO
